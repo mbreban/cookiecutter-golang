b1a2205dc3ae4002405b3f1f8af8800571855b10
mbreban <breban.mathieu@gmail.com>
2024-03-21 16:22:03 +0100

    Remove go dep feature

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Powered by [Cookiecutter](https://github.com/audreyr/cookiecutter), Cookiecutter
 ## Features
 
 - Generous `Makefile` with management commands
-- Uses `go dep` (with optional go module support *requires go 1.11*)
 - injects build time and git hash at build time.
 
 ## Optional Integrations
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -102,13 +102,7 @@ else:
     remove_file(".travis.yml")
     remove_circleci_files()
 
-# 6. Remove files depending on selection of mod or dep
-if '{{ cookiecutter.go_mod_or_dep}}'.lower() == 'mod':
-    remove_file("Gopkg.toml")
-else:
-    remove_file("go.mod")
-
-# 7. Initialize Git (should be run after all file have been modified or deleted)
+# 6. Initialize Git (should be run after all file have been modified or deleted)
 if '{{ cookiecutter.use_git }}'.lower() == 'y':
     init_git()
 else:
